5dd230873d8dc372d996d52966a4be348077a3dc
KimSeungHan <rlatmdgks4@naver.com>
2026-03-06 09:15:01 +0900

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(NULL))); // 실행할때마다 다른 난수 생성을 위해서
+	vector <int>Player;
+	vector <int>Computer;
+	int Pnum;
+	int Cnum;
+	int Ptotal = 0;
+	int Ctotal = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		Pnum = rand() % 10 + 1;
+		Cnum = rand() % 10 + 1;
+		Player.push_back(Pnum);
+		Computer.push_back(Cnum);
+	}
+
+	for (auto player : Player)
+	{
+		cout << "Player card: " << player << endl;
+		Ptotal += player;
+	}
+	for (auto computer : Computer)
+	{
+		cout << "Computer card: " << computer << endl;
+		Ctotal += computer;
+	}
+
+	if (Ptotal < 22 && Ctotal < 22)
+	{
+		if(Ptotal > Ctotal)
+		{
+			cout << "Player wins!" << endl;
+		}
+		else if (Ptotal < Ctotal)
+		{
+			cout << "Computer wins!" << endl;
+		}
+		else
+		{
+			cout << "Player wins!" << endl;
+		}
+	}
+	else if (Ptotal > 21 && Ctotal > 21)
+	{
+		cout << "It's a tie!" << endl;
+	}
+	else if (Ptotal > 21 && Ctotal < 22)
+	{
+		cout << "Computer wins!" << endl;
+	}
+	else if (Ptotal < 22 && Ctotal > 21)
+	{
+		cout << "Player wins!" << endl;
+	}
+
+}
